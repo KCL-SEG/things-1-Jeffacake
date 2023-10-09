da000117863b3bea844baa1941214045c13aa6ed
Jeff Jones <jeffbjones777@gmail.com>
2023-10-09 13:31:42 +0100

    A different approach

--- things/models.py
+++ things/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext as _
 
-# Create your models here.
 class Thing(models.Model):
-    name = models.CharField(unique=True, blank=False, max_length=30)
-    description = models.CharField(blank=True, max_length=120)
-    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30,
+        verbose_name=_('Name')
+    )
+    description = models.CharField(
+        blank=True,
+        max_length=120,
+        verbose_name=_('Description')
+    )
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        verbose_name=_('Quantity')
+    )
 
+    def __str__(self):
+        return self.name
 
-
-
-
-
-
+    class Meta:
+        verbose_name = _('Thing')
+        verbose_name_plural = _('Things')
