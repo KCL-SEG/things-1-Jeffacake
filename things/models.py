95357f08c1dcd950b7b1bbdd151c5b1aa84c981b
Jeff Jones <jeffbjones777@gmail.com>
2023-10-09 13:22:33 +0100

    Made name unique

--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, blank=False)
+    name = models.CharField(max_length=30, blank=False, unique=True)
     description = models.CharField(blank=True, max_length=120)
     quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
 
