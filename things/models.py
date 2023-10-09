8a1dda23c0d1f10eabaeb70def58db66d5a5d4d6
Jeff Jones <jeffbjones777@gmail.com>
2023-10-09 13:14:57 +0100

    Trying to debug

--- things/models.py
+++ things/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     description = models.TextField()
     quantity = models.IntegerField()
+
+
+
 
 
 
